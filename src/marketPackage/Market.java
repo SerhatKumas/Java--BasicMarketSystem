264d9ca157ca869847f03c11c2d05f270811ffbb
Serhat Kumas <serhatkumas@outlook.com>
2022-12-06 14:30:24 +0300

    Market class is developed

---
+++ src/marketPackage/Market.java
@@ -0,0 +1,29 @@
+package marketPackage;
+
+public class Market {
+    private String marketId;
+    private String name;
+    private String location;
+    private String ownerId;
+
+    public Market() {
+        this.marketId = "";
+        this.name = "";
+        this.location = "";
+        this.ownerId = "";
+    }
+
+    public Market(String marketId, String name, String location, String ownerId) {
+        this.marketId = marketId;
+        this.name = name;
+        this.location = location;
+        this.ownerId = ownerId;
+    }
+
+    public Market(Market market) {
+        this.marketId = market.marketId;
+        this.name = market.name;
+        this.location = market.location;
+        this.ownerId = market.ownerId;
+    }
+}
