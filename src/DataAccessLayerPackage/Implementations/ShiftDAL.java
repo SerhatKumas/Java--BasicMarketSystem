9dd6678b0c39b7443c6d20b8306c13df99fd07a5
Serhat Kumas <serhat.kumas@hotmail.com>
2022-12-16 02:15:51 +0300

    Shift Dal is implemented

--- src/DataAccessLayerPackage/Implementations/ShiftDAL.java
+++ src/DataAccessLayerPackage/Implementations/ShiftDAL.java
@@ -3,43 +3,73 @@ package DataAccessLayerPackage.Implementations;
 import AssetsPackage.ShiftPackage.Shift;
 import DataAccessLayerPackage.Interfaces.IShiftDAL;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
 
 public class ShiftDAL implements IShiftDAL {
 
     @Override
-    public void displayByDate(Statement statement, Date date) {
-
+    public void displayByDate(Statement statement, String date) {
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM shifttable WHERE ShiftDate = '"+date+"'");
+            while(resultSet.next()){
+                System.out.println("Shift Id : "+resultSet.getString(1) + " \nEmployee Id : "+resultSet.getString(2)+" \nEmployee Name : "+resultSet.getString(3)+" \nStart Time : "+resultSet.getString(4)+" \nEnd Time : "+resultSet.getString(5)+" \nShift Day : "+resultSet.getDate(6));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
     public void displayByTimeRange(Statement statement, String startTime, String endTime) {
-
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM shifttable WHERE TimeOfStart >= '"+startTime+"' AND TimeOfEnd <= '"+endTime+"'");
+            while(resultSet.next()){
+                System.out.println("Shift Id : "+resultSet.getString(1) + " \nEmployee Id : "+resultSet.getString(2)+" \nEmployee Name : "+resultSet.getString(3)+" \nStart Time : "+resultSet.getString(4)+" \nEnd Time : "+resultSet.getString(5)+" \nShift Day : "+resultSet.getDate(6));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
     public void displayByEmployeeId(Statement statement, String id) {
-
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM shifttable WHERE EmployeeId = '"+id+"'");
+            while(resultSet.next()){
+                System.out.println("Shift Id : "+resultSet.getString(1) + " \nEmployee Id : "+resultSet.getString(2)+" \nEmployee Name : "+resultSet.getString(3)+" \nStart Time : "+resultSet.getString(4)+" \nEnd Time : "+resultSet.getString(5)+" \nShift Day : "+resultSet.getDate(6));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
     public void displayByEmployeeName(Statement statement, String name) {
-
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM shifttable WHERE EmployeeName = '"+name+"'");
+            while(resultSet.next()){
+                System.out.println("Shift Id : "+resultSet.getString(1) + " \nEmployee Id : "+resultSet.getString(2)+" \nEmployee Name : "+resultSet.getString(3)+" \nStart Time : "+resultSet.getString(4)+" \nEnd Time : "+resultSet.getString(5)+" \nShift Day : "+resultSet.getDate(6));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
-    public Shift createShift(Statement statement, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd) {
-        return null;
-    }
+    public void createShift(Statement statement, String shiftId, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd) {
+        try {
+            boolean isAdded = statement.execute("INSERT INTO `shifttable` (`ShiftId`, `EmployeeId`, `EmployeeName`, `TimeOfStart`, `TimeOfEnd`, `ShiftDate`) VALUES ('"+shiftId+"', '"+employeeId+"', '"+employeeName+"', '"+timeOfStart+"', '"+timeOfEnd+"', '"+shiftDay+"')");
+            if(isAdded){
+                System.out.println("Shift can not be added.");
 
-    @Override
-    public void addShift(Statement statement, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd) {
-
-    }
-
-    @Override
-    public void printShift(Shift shift) {
-
+            }
+            else{
+                System.out.println("Shift is added.");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- src/DataAccessLayerPackage/Interfaces/IShiftDAL.java
+++ src/DataAccessLayerPackage/Interfaces/IShiftDAL.java
@@ -6,14 +6,10 @@ import java.sql.Statement;
 import java.util.Date;
 
 public interface IShiftDAL {
-    void displayByDate(Statement statement, Date date);
+    void displayByDate(Statement statement, String date);
     void displayByTimeRange(Statement statement, String startTime, String endTime);
     void displayByEmployeeId(Statement statement, String id);
     void displayByEmployeeName(Statement statement, String name);
 
-    Shift createShift(Statement statement, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd);
-
-    void addShift(Statement statement, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd);
-
-    void printShift(Shift shift);
+    void createShift(Statement statement, String shiftId, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd);
 }
