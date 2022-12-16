4c152081af743cdcc8da4ab273cff09ef36c38a3
Serhat Kumas <serhat.kumas@hotmail.com>
2022-12-17 02:04:02 +0300

    Owner manager is implemented

--- src/BusinessLayer/Implementations/OwnerManager.java
+++ src/BusinessLayer/Implementations/OwnerManager.java
@@ -1,40 +1,44 @@
 package BusinessLayer.Implementations;
 
 import BusinessLayer.Interfaces.IOwnerManager;
+import DataAccessLayerPackage.Implementations.OwnerDAL;
+
+import java.sql.Statement;
+import java.util.Date;
 
 public class OwnerManager implements IOwnerManager {
     @Override
-    public void displayAllEmployees() {
-
+    public void displayAllOwners(OwnerDAL ownerDal, Statement statement) {
+    ownerDal.displayAll(statement);
     }
 
     @Override
-    public void displayOwnerById() {
-
+    public void displayOwnerById(OwnerDAL ownerDal, Statement statement, String ownerId) {
+    ownerDal.displayById(statement,ownerId);
     }
 
     @Override
-    public void displayOwnersByName() {
-
+    public void displayOwnersByName(OwnerDAL ownerDal, Statement statement, String ownerName) {
+    ownerDal.displayByName(statement,ownerName);
     }
 
     @Override
-    public void displayOwnerBySsn() {
-
+    public void displayOwnerBySsn(OwnerDAL ownerDal, Statement statement, String ownerSsn) {
+    ownerDal.displayBySsn(statement,ownerSsn);
     }
 
     @Override
-    public void deleteOwnerById() {
-
+    public void deleteOwnerById(OwnerDAL ownerDal, Statement statement, String ownerId) {
+    ownerDal.deleteById(statement,ownerId);
     }
 
     @Override
-    public void deleteOwnerBySsn() {
-
+    public void deleteOwnerBySsn(OwnerDAL ownerDal, Statement statement, String ownerSsn) {
+    ownerDal.deleteBySsn(statement,ownerSsn);
     }
 
     @Override
-    public void addNewOwner() {
-
+    public void addNewOwner(OwnerDAL ownerDal, Statement statement, String name, String phone, Date dateOfBirth, String ssn, String address, String workerId, String marketId, String workerPassword, float sharePercentage) {
+    ownerDal.createNewOwner(statement, name, phone, dateOfBirth, ssn, address, workerId, marketId, workerPassword, sharePercentage);
     }
 }
--- src/BusinessLayer/Interfaces/IOwnerManager.java
+++ src/BusinessLayer/Interfaces/IOwnerManager.java
@@ -1,13 +1,18 @@
 package BusinessLayer.Interfaces;
 
+import DataAccessLayerPackage.Implementations.OwnerDAL;
+
+import java.sql.Statement;
+import java.util.Date;
+
 public interface IOwnerManager {
 
-    void displayAllEmployees();
-    void displayOwnerById();
-    void displayOwnersByName();
-    void displayOwnerBySsn();
-    void deleteOwnerById();
-    void deleteOwnerBySsn();
-    void addNewOwner();
+    void displayAllOwners(OwnerDAL ownerDal, Statement statement);
+    void displayOwnerById(OwnerDAL ownerDal, Statement statement, String ownerId);
+    void displayOwnersByName(OwnerDAL ownerDal, Statement statement, String ownerName);
+    void displayOwnerBySsn(OwnerDAL ownerDal, Statement statement, String ownerSsn);
+    void deleteOwnerById(OwnerDAL ownerDal, Statement statement, String ownerId);
+    void deleteOwnerBySsn(OwnerDAL ownerDal, Statement statement, String ownerSsn);
+    void addNewOwner(OwnerDAL ownerDal, Statement statement, String name, String phone, Date dateOfBirth, String ssn, String address, String workerId, String marketId, String workerPassword, float sharePercentage);
 }
 
