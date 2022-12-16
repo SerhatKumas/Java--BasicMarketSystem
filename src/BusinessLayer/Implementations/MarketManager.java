50f75efa7ef7d742b3460921513a241e5c1f7578
Serhat Kumas <serhat.kumas@hotmail.com>
2022-12-17 01:58:09 +0300

    Market manager is implemented

--- src/BusinessLayer/Implementations/MarketManager.java
+++ src/BusinessLayer/Implementations/MarketManager.java
@@ -1,40 +1,43 @@
 package BusinessLayer.Implementations;
 
 import BusinessLayer.Interfaces.IMarketManager;
+import DataAccessLayerPackage.Interfaces.IMarketDAL;
+
+import java.sql.Statement;
 
 public class MarketManager implements IMarketManager {
     @Override
-    public void displayAllMarkets() {
-
+    public void displayAllMarkets(IMarketDAL marketDal, Statement statement) {
+    marketDal.displayAllMarkets(statement);
     }
 
     @Override
-    public void displayMarketByMarketId() {
-
+    public void displayMarketByMarketId(IMarketDAL marketDal, Statement statement, String marketId) {
+    marketDal.displayMarketByMarketId(statement,marketId);
     }
 
     @Override
-    public void displayMarketsByLocation() {
-
+    public void displayMarketsByLocation(IMarketDAL marketDal, Statement statement, String marketLocation) {
+    marketDal.displayMarketsByLocation(statement,marketLocation);
     }
 
     @Override
-    public void displayMarketsByName() {
-
+    public void displayMarketsByName(IMarketDAL marketDal, Statement statement, String marketName) {
+    marketDal.displayMarketsByName(statement,marketName);
     }
 
     @Override
-    public void displayMarketsByOwnerId() {
-
+    public void displayMarketsByOwnerId(IMarketDAL marketDal, Statement statement, String ownerId) {
+    marketDal.displayMarketsByOwnerId(statement,ownerId);
     }
 
     @Override
-    public void deleteMarketByMarketId() {
-
+    public void deleteMarketByMarketId(IMarketDAL marketDal, Statement statement, String marketId) {
+    marketDal.deleteMarketByMarketId(statement,marketId);
     }
 
     @Override
-    public void addNewMarket() {
-
+    public void addNewMarket(IMarketDAL marketDal, Statement statement, String marketId, String name, String location, String ownerId) {
+    marketDal.createMarket(statement, marketId, name, location, ownerId);
     }
 }
--- src/BusinessLayer/Interfaces/IMarketManager.java
+++ src/BusinessLayer/Interfaces/IMarketManager.java
@@ -1,11 +1,15 @@
 package BusinessLayer.Interfaces;
 
+import DataAccessLayerPackage.Interfaces.IMarketDAL;
+
+import java.sql.Statement;
+
 public interface IMarketManager {
-    void displayAllMarkets();
-    void displayMarketByMarketId();
-    void displayMarketsByLocation();
-    void displayMarketsByName();
-    void displayMarketsByOwnerId();
-    void deleteMarketByMarketId();
-    void addNewMarket();
+    void displayAllMarkets(IMarketDAL marketDal, Statement statement);
+    void displayMarketByMarketId(IMarketDAL marketDal, Statement statement, String marketId);
+    void displayMarketsByLocation(IMarketDAL marketDal, Statement statement, String marketLocation);
+    void displayMarketsByName(IMarketDAL marketDal, Statement statement, String marketName);
+    void displayMarketsByOwnerId(IMarketDAL marketDal, Statement statement, String ownerId);
+    void deleteMarketByMarketId(IMarketDAL marketDal, Statement statement, String marketId);
+    void addNewMarket(IMarketDAL marketDal, Statement statement, String marketId, String name, String location, String ownerId);
 }
