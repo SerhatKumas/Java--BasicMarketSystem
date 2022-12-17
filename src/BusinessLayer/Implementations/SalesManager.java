3c747a9f969a6fe5e360c94030cf06f597bc0491
Serhat Kumas <serhat.kumas@hotmail.com>
2022-12-17 12:08:34 +0300

    Sales manager is developed

--- src/BusinessLayer/Implementations/SalesManager.java
+++ src/BusinessLayer/Implementations/SalesManager.java
@@ -1,40 +1,43 @@
 package BusinessLayer.Implementations;
 
 import BusinessLayer.Interfaces.ISalesManager;
+import DataAccessLayerPackage.Interfaces.ISalesDAL;
+
+import java.sql.Statement;
 
 public class SalesManager implements ISalesManager {
     @Override
-    public void displayAllSales() {
-
+    public void displayAllSales(ISalesDAL salesDal, Statement statement) {
+    salesDal.displayAllSales(statement);
     }
 
     @Override
-    public void displaySaleById() {
-
+    public void displaySaleById(ISalesDAL salesDal, Statement statement, String salesId) {
+    salesDal.displaySaleByID(statement,salesId);
     }
 
     @Override
-    public void displaySalesByShiftId() {
-
+    public void displaySalesByShiftId(ISalesDAL salesDal, Statement statement, String shiftId) {
+    salesDal.displaySalesByShiftId(statement,shiftId);
     }
 
     @Override
-    public void displaySalesByWorkerId() {
-
+    public void displaySalesByWorkerId(ISalesDAL salesDal, Statement statement, String workerId) {
+    salesDal.displaySalesByWorkerId(statement,workerId);
     }
 
     @Override
-    public void displaySalesByProductId() {
-
+    public void displaySalesByProductId(ISalesDAL salesDal, Statement statement, String productId) {
+    salesDal.displaySalesByProductId(statement,productId);
     }
 
     @Override
-    public void deleteSalesBySalesId() {
-
+    public void deleteSalesBySalesId(ISalesDAL salesDal, Statement statement, String salesId) {
+    salesDal.deleteSalesById(statement,salesId);
     }
 
     @Override
-    public void addNewSales() {
-
+    public void addNewSales(ISalesDAL salesDal, Statement statement, String salesId, String shiftId, String workerId, String productId) {
+    salesDal.createSales(statement, salesId, shiftId, workerId, productId);
     }
 }
--- src/BusinessLayer/Interfaces/ISalesManager.java
+++ src/BusinessLayer/Interfaces/ISalesManager.java
@@ -1,11 +1,15 @@
 package BusinessLayer.Interfaces;
 
+import DataAccessLayerPackage.Interfaces.ISalesDAL;
+
+import java.sql.Statement;
+
 public interface ISalesManager {
-    void displayAllSales();
-    void displaySaleById();
-    void displaySalesByShiftId();
-    void displaySalesByWorkerId();
-    void displaySalesByProductId();
-    void deleteSalesBySalesId();
-    void addNewSales();
+    void displayAllSales(ISalesDAL salesDal, Statement statement);
+    void displaySaleById(ISalesDAL salesDal, Statement statement, String salesId);
+    void displaySalesByShiftId(ISalesDAL salesDal, Statement statement, String shiftId);
+    void displaySalesByWorkerId(ISalesDAL salesDal, Statement statement, String workerId);
+    void displaySalesByProductId(ISalesDAL salesDal, Statement statement, String productId);
+    void deleteSalesBySalesId(ISalesDAL salesDal, Statement statement, String salesId);
+    void addNewSales(ISalesDAL salesDal, Statement statement, String salesId, String shiftId, String workerId, String productId);
 }
