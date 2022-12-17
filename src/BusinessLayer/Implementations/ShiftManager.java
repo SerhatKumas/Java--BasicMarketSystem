3e8a5f61376615ab56fa9dfbe5a99fb4e999d986
Serhat Kumas <serhat.kumas@hotmail.com>
2022-12-17 12:33:58 +0300

    Shift manager is implemented

--- src/BusinessLayer/Implementations/ShiftManager.java
+++ src/BusinessLayer/Implementations/ShiftManager.java
@@ -1,40 +1,44 @@
 package BusinessLayer.Implementations;
 
 import BusinessLayer.Interfaces.IShiftManager;
+import DataAccessLayerPackage.Interfaces.IShiftDAL;
+
+import java.sql.Statement;
+import java.util.Date;
 
 public class ShiftManager implements IShiftManager {
     @Override
-    public void displayAllShifts() {
-
+    public void displayAllShifts(IShiftDAL shiftDal, Statement statement) {
+    shiftDal.displayAllShifts(statement);
     }
 
     @Override
-    public void displayShiftByShiftId() {
-
+    public void displayShiftByShiftId(IShiftDAL shiftDal, Statement statement, String shiftId) {
+    shiftDal.displayByShiftId(statement,shiftId);
     }
 
     @Override
-    public void displayShiftsByWorkerId() {
-
+    public void displayShiftsByWorkerId(IShiftDAL shiftDal, Statement statement, String workerId) {
+    shiftDal.displayByEmployeeId(statement,workerId);
     }
 
     @Override
-    public void displayShiftsByWorkerName() {
-
+    public void displayShiftsByWorkerName(IShiftDAL shiftDal, Statement statement, String workerName) {
+    shiftDal.displayByEmployeeName(statement,workerName);
     }
 
     @Override
-    public void displayShiftsByTimeRange() {
-
+    public void displayShiftsByTimeRange(IShiftDAL shiftDal, Statement statement, String startTime, String endTime) {
+    shiftDal.displayByTimeRange(statement,startTime,endTime);
     }
 
     @Override
-    public void displayShiftsByDate() {
-
+    public void displayShiftsByDate(IShiftDAL shiftDal, Statement statement, String date) {
+    shiftDal.displayByDate(statement,date);
     }
 
     @Override
-    public void addNewShift() {
-
+    public void addNewShift(IShiftDAL shiftDal, Statement statement, String shiftId, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd) {
+    shiftDal.createShift(statement, shiftId, employeeId, employeeName, shiftDay, timeOfStart, timeOfEnd);
     }
 }
--- src/BusinessLayer/Interfaces/IShiftManager.java
+++ src/BusinessLayer/Interfaces/IShiftManager.java
@@ -1,11 +1,16 @@
 package BusinessLayer.Interfaces;
 
+import DataAccessLayerPackage.Interfaces.IShiftDAL;
+
+import java.sql.Statement;
+import java.util.Date;
+
 public interface IShiftManager {
-    void displayAllShifts();
-    void displayShiftByShiftId();
-    void displayShiftsByWorkerId();
-    void displayShiftsByWorkerName();
-    void displayShiftsByTimeRange();
-    void displayShiftsByDate();
-    void addNewShift();
+    void displayAllShifts(IShiftDAL shiftDal, Statement statement);
+    void displayShiftByShiftId(IShiftDAL shiftDal, Statement statement, String shiftId);
+    void displayShiftsByWorkerId(IShiftDAL shiftDal, Statement statement, String workerId);
+    void displayShiftsByWorkerName(IShiftDAL shiftDal, Statement statement, String workerName);
+    void displayShiftsByTimeRange(IShiftDAL shiftDal, Statement statement, String startTime, String endTime);
+    void displayShiftsByDate(IShiftDAL shiftDal, Statement statement, String date);
+    void addNewShift(IShiftDAL shiftDal, Statement statement, String shiftId, String employeeId, String employeeName, Date shiftDay, String timeOfStart, String timeOfEnd);
 }
