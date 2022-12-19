b9644cfe60499a04f4bf5f6b9cdbe3b4278a7399
Serhat Kumas <serhatkumas@outlook.com>
2022-12-19 22:45:13 +0300

    Owner interface template is developed

--- src/UserInterfacesPackage/Implementation/OwnerInterface.java
+++ src/UserInterfacesPackage/Implementation/OwnerInterface.java
@@ -5,42 +5,48 @@ import UserInterfacesPackage.Interfaces.IOwnerInterface;
 public class OwnerInterface implements IOwnerInterface {
     @Override
     public void printEmployeeMenu() {
-
+        System.out.println("\n--------- Product Menu -----------");
+        System.out.println("1)Display employee by Id\n2)Display employee by name\n3)Display all employees\n4)Display employee by ssn\n5)Add employee\n6)Delete employee by id\n7)Delete employee by ssn\n8)Previous menu");
     }
 
     @Override
     public void printProductMenu() {
-
+        System.out.println("\n--------- Product Menu -----------");
+        System.out.println("1)Display product by Id\n2)Display product by name\n3)Display all products\n4)Display product by category\n5)Add product\n6)Delete product by product id\n7)Previous menu");
     }
 
     @Override
     public void printShiftMenu() {
-
+        System.out.println("\n--------- Shift Menu -----------");
+        System.out.println("1)Display shift by Id\n2)Display shift by employee id\n3)Display all shifts\n4)Display shift by employee id\n5)Display shift by employee name\n6)Display shift by time range\n7)Previous menu");
     }
 
     @Override
     public void printSalesMenu() {
-
+        System.out.println("\n--------- Sales Menu -----------");
+        System.out.println("1)Display sales by idd\n2)Display sales by employee id\n3)Display all sales\n4)Display sales by shift id\n5)Display sales by product id\n6)Delete sales by id\n7)Previous menu");
     }
 
     @Override
     public void printWelcomeMessage() {
-
+        System.out.println("Welcome to the owner system");
     }
 
     @Override
     public void printMainMenu() {
-
+        System.out.println("\n--------- Main Menu -----------");
+        System.out.println("1)Cashier Menu\n2)Product Menu\n3)Sales Menu\n4)Shift Menu\n5)Employee Menu\n6)Log out");
     }
 
     @Override
     public void printLogoutMessage() {
-
+        System.out.println("Good bye..\nYou are logged out and you're shift has finished.");
     }
 
     @Override
     public void printCashierMenu() {
-
+        System.out.println("\n--------- Cashier Menu -----------");
+        System.out.println("1)Sell product by Id\n2)Refund product by Id\n3)Previous menu");
     }
 
     public void ownerProgramRunner() {
